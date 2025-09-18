ee5f1f97fea1acca80dfbf88260eeda9e0c17b9f
TheMattWang <mat.wang25@gmail.com>
2025-09-18 12:35:50 -0500

    basic agent from langgraph

--- agent/agent.py
+++ agent/agent.py
@@ -7,6 +7,9 @@ from langgraph.graph.message import add_messages
 
 from dotenv import load_dotenv
 
+from langchain_community.utilities import SQLDatabase
+from langchain_community.agent_toolkits import SQLDatabaseToolkit
+
 load_dotenv()
 
 class State(TypedDict):
@@ -44,16 +47,74 @@ def stream_graph_updates(user_input: str):
             print("Assistant:", value["messages"][-1].content)
 
 
-while True:
-    try:
-        user_input = input("User: ")
-        if user_input.lower() in ["quit", "exit", "q"]:
-            print("Goodbye!")
-            break
-        stream_graph_updates(user_input)
-    except:
-        # fallback if input() is not available
-        user_input = "What do you know about LangGraph?"
-        print("User: " + user_input)
-        stream_graph_updates(user_input)
-        break
+db = SQLDatabase.from_uri("sqlite:///rides.sqlite")
+
+print(f"Dialect: {db.dialect}")
+print(f"Available tables: {db.get_usable_table_names()}")
+print(f'Sample output: {db.run("SELECT * FROM Trips LIMIT 5;")}')
+
+toolkit = SQLDatabaseToolkit(db=db, llm=llm)
+tools = toolkit.get_tools()
+
+from langgraph.prebuilt import create_react_agent
+
+system_prompt = """
+You are an agent designed to interact with a SQL database.
+Given an input question, create a syntactically correct {dialect} query to run,
+then look at the results of the query and return the answer. Unless the user
+specifies a specific number of examples they wish to obtain, always limit your
+query to at most {top_k} results.
+
+You can order the results by a relevant column to return the most interesting
+examples in the database. Never query for all the columns from a specific table,
+only ask for the relevant columns given the question.
+
+You MUST double check your query before executing it. If you get an error while
+executing a query, rewrite the query and try again.
+
+DO NOT make any DML statements (INSERT, UPDATE, DELETE, DROP etc.) to the
+database.
+
+To start you should ALWAYS look at the tables in the database to see what you
+can query. Do NOT skip this step.
+
+Then you should query the schema of the most relevant tables.
+""".format(
+    dialect=db.dialect,
+    top_k=5,
+)
+
+agent = create_react_agent(
+    llm,
+    tools,
+    prompt=system_prompt,
+)
+
+print("*"*10)
+print("Finished")
+
+question = "What is the most common place for drop offs"
+
+for step in agent.stream(
+    {"messages": [{"role": "user", "content": question}]},
+    stream_mode="values",
+):
+    step["messages"][-1].pretty_print()
+
+# for tool in tools:
+#     print(f"{tool.name}: {tool.description}\n")
+
+
+# while True:
+#     try:
+#         user_input = input("User: ")
+#         if user_input.lower() in ["quit", "exit", "q"]:
+#             print("Goodbye!")
+#             break
+#         stream_graph_updates(user_input)
+#     except:
+#         # fallback if input() is not available
+#         user_input = "What do you know about LangGraph?"
+#         print("User: " + user_input)
+#         stream_graph_updates(user_input)
+#         break
--- plan.md
+++ plan.md
@@ -1,12 +1,14 @@
 Tuesday: Basic Agent with Cabapilities
 
 Just use GPT for base case, Two Papers citing GPT 4 is just goated:
+Going with Gemini don't wanna spend money lol
 - https://arxiv.org/html/2505.20139v1
 - https://www.openproceedings.org/2025/conf/edbt/paper-211.pdf
 
 Going to use LangGraph
 
-Wednesday: Integrate With SQL
+Thursday: Integrate With SQL
+- Going with SQLlite, easy to integrate in small amount of data
 - Just build the thing
 Thursday: Frontend Integration
 
